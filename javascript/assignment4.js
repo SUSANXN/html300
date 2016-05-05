130496f423a6cc61cdb310e4bf2f89002c002346
susanxn <sxnguyen@uw.edu>
2016-05-04 21:36:42 -0700

    add input fields with geomap, tester

--- javascript/assignment4.js
+++ javascript/assignment4.js
@@ -33,6 +33,34 @@ if ( navGeoLoc )
             timeout: 60000 //1 min
         } );
 }
+<script type="text/javascript">
+		var lati =0;
+		var longi =0;
+		function getPosition(position) 
+		{
+			lati = position.coords.latitude;
+			longi = position.coords.longitude;
+		}
+
+		function writeLati(form)
+		{
+			if (navigator.geolocation) { 
+				navigator.geolocation.getCurrentPosition(getPosition);
+				lati = position.coords.latitude;
+				longi = position.coords.longitude;
+				form.latitude.value = lati;
+				form.longitude.value = longi;
+			} else {
+				form.latitude.value = "3";
+				form.longitude.value = "5";
+				document.write("Your shitty browser can't do geolocation. Get Firefox 3.5.");
+			}
+
+		}
+
+
+
+	</script>
 else
 {
     console.log( 'No geolocation support' );
